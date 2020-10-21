b4385b170e44c2ef60e9c76abd689157e46be2be
Mikita Mironenka <m.mironenka@gmail.com>
2020-10-21 11:41:09 +0300

    4. Form с композиционным объектом[305522#317172]

---
+++ src/main/java/ru/job4j/accident/model/AccidentType.java
@@ -0,0 +1,18 @@
+package ru.job4j.accident.model;
+
+import lombok.Data;
+
+@Data
+public class AccidentType {
+
+    private String name;
+    private int id;
+
+
+    public static AccidentType of(int id, String name) {
+        AccidentType type = new AccidentType();
+        type.id = id;
+        type.name = name;
+        return type;
+    }
+}
--- src/main/java/ru/job4j/accident/repository/AccidentMem.java
+++ src/main/java/ru/job4j/accident/repository/AccidentMem.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.springframework.stereotype.Repository;
 import ru.job4j.accident.model.Accident;
 
+
 import java.util.HashMap;
 import java.util.List;
 
