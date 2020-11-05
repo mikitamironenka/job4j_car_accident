de49d4f11ad9ca489a7479e87e9504827ed5c3a3
Mikita Mironenka <m.mironenka@gmail.com>
2020-11-05 16:41:31 +0300

    2. Spring Data[296073#317192]

--- src/main/java/ru/job4j/accident/WebInit.java
+++ src/main/java/ru/job4j/accident/WebInit.java
@@ -5,6 +5,7 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 import ru.job4j.accident.config.HbnConfig;
+import ru.job4j.accident.config.SpringDataConfig;
 import ru.job4j.accident.config.WebConfig;
 
 import javax.servlet.FilterRegistration;
@@ -15,7 +16,7 @@ public class WebInit implements WebApplicationInitializer {
 
     public void onStartup(ServletContext servletCxt) {
         AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
-        ac.register(WebConfig.class, HbnConfig.class);
+        ac.register(WebConfig.class, SpringDataConfig.class);
         ac.refresh();
         CharacterEncodingFilter filter = new CharacterEncodingFilter();
         filter.setEncoding("UTF-8");
--- src/main/java/ru/job4j/accident/service/AccidentService.java
+++ src/main/java/ru/job4j/accident/service/AccidentService.java
@@ -4,45 +4,73 @@ import org.springframework.stereotype.Service;
 import ru.job4j.accident.model.Accident;
 import ru.job4j.accident.model.AccidentType;
 import ru.job4j.accident.model.Rule;
-import ru.job4j.accident.repository.AccidentHibernateTemplate;
+import ru.job4j.accident.repository.AccidentRepository;
+import ru.job4j.accident.repository.AccidentTypeRepository;
+import ru.job4j.accident.repository.RuleRepository;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AccidentService {
 
-    private AccidentHibernateTemplate accidentTemplate;
+    private final AccidentRepository accidentRepository;
+    private final AccidentTypeRepository accidentTypeRepository;
+    private final RuleRepository ruleRepository;
 
-    public AccidentService(AccidentHibernateTemplate accidentTemplate) {
-        this.accidentTemplate = accidentTemplate;
+    public AccidentService(AccidentRepository accidentRepository, AccidentTypeRepository accidentTypeRepository, RuleRepository ruleRepository) {
+        this.accidentRepository = accidentRepository;
+        this.accidentTypeRepository = accidentTypeRepository;
+        this.ruleRepository = ruleRepository;
     }
 
     public List<Accident> getAccidents() {
-        return this.accidentTemplate.getAll();
+        List<Accident> res = new ArrayList<>();
+        this.accidentRepository.findAll().forEach(res :: add);
+        return res;
     }
 
     public List<AccidentType> getTypes() {
-        return this.accidentTemplate.getTypes();
+        List<AccidentType> res = new ArrayList<>();
+        this.accidentTypeRepository.findAll().forEach(res :: add);
+        return res;
     }
 
     public List<Rule> getRules() {
-        return this.accidentTemplate.getRules();
+        List<Rule> res = new ArrayList<>();
+        this.ruleRepository.findAll().forEach(res :: add);
+        return res;
     }
 
     public void create(Accident accident) {
-        this.accidentTemplate.save(accident);
+        this.accidentRepository.save(accident);
     }
 
     public void update(Accident accident) {
-        this.accidentTemplate.update(accident);
+        Optional<Accident> accdnt = accidentRepository.findById(accident.getId());
+        if (accdnt.isPresent()){
+            Accident accidentTemp = accdnt.get();
+            if (accident.getName() != null)
+                accidentTemp.setName(accident.getName());
+            if (accident.getText() != null)
+                accidentTemp.setText(accident.getText());
+            if (accident.getAddress() != null)
+                accidentTemp.setAddress(accident.getAddress());
+            this.accidentRepository.save(accidentTemp);
+        }
     }
 
-    public Accident findById(int id) {
-        return this.accidentTemplate.findAccidentById(id);
+    public Optional<Accident> findById(int id) {
+        return this.accidentRepository.findById(id);
     }
 
     public void addRulesToAccident(Accident accident, String[] rIds) {
         for (String id : rIds) {
-            accident.getRules().add(accidentTemplate.getRuleById(Integer.parseInt(id)));
+            accident.getRules()
+                .add(ruleRepository
+                    .findById(Integer.parseInt(id))
+                    .get());
         }
     }
 }
