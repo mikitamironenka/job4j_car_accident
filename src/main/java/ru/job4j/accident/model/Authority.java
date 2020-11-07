f7012e7edf7547ede57628f773d6fed33675fd9d
Mikita Mironenka <m.mironenka@gmail.com>
2020-11-07 10:56:34 +0300

    0. Spring Security[6879#317186]

---
+++ src/main/java/ru/job4j/accident/model/Authority.java
@@ -0,0 +1,13 @@
+//package ru.job4j.accident.model;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//
+//@Entity
+//@Table(name = "authorities")
+//public class Authority {
+//
+//
+//    private String authority;
+//
+//}
---
+++ src/main/java/ru/job4j/accident/model/User.java
@@ -0,0 +1,18 @@
+//package ru.job4j.accident.model;
+//
+//import lombok.Data;
+//
+//import javax.persistence.*;
+//
+//@Entity
+//@Data
+//@Table(name = "users")
+//public class User {
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private String username;
+//    private String password;
+//    private boolean enabled;
+//
+//}
