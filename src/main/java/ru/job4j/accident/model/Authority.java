90ba9b660acc1563e7d342159373a9536323efe1
Mikita Mironenka <m.mironenka@gmail.com>
2020-11-10 13:54:14 +0300

    1. Авторизация через JDBC[2094#317185]

--- src/main/java/ru/job4j/accident/model/Authority.java
+++ src/main/java/ru/job4j/accident/model/Authority.java
@@ -1,13 +1,17 @@
-//package ru.job4j.accident.model;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.Table;
-//
-//@Entity
-//@Table(name = "authorities")
-//public class Authority {
-//
-//
-//    private String authority;
-//
-//}
+package ru.job4j.accident.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "authorities")
+public class Authority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String authority;
+
+}
